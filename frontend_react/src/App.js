74613e9e714f6a3dae204c62157eddd1bd997227
WAJDI RIAHI <rwajdi2020@gmail.com>
2023-08-10 16:04:35 +0200

    file & folder structure

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
+import "./App.scss";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
